2e1fbe3af36987ad68ebe95573dedd7ddfa30ba9
ailisp <icerove@gmail.com>
2016-09-22 23:18:11 +0800

    Create subleq.lisp

---
+++ HW1/subleq.lisp
@@ -0,0 +1,25 @@
+;;;; a subleq interpreter
+(defparameter *mem* (make-array 4))
+(eval-when (:load-toplevel :eval-toplevel :execute)
+  (defmacro subleq (a b c)
+  `(progn
+     (decf (aref *mem* ,a) (aref *mem* ,b))
+     (print *mem*)
+     (if (<= (aref *mem* ,a) 0) (go ,c)))))
+
+(let ((a 0) (b 1) (c 2) (d 3))
+  (setf (aref *mem* a) 7
+	(aref *mem* b) -9
+	(aref *mem* c) 4
+	(aref *mem* d) 3)
+  (tagbody
+   L0 (subleq C C L1)
+   L1 (subleq D D L2)
+   L2 (subleq A B L6)
+   L3 (subleq D B L4)
+   L4 (subleq C D L5)
+   L5 (subleq D D L9)
+   L6 (subleq D A L7)
+   L7 (subleq C D L8)
+   L8 (subleq D D L9)
+   L9 (print *mem*)))
