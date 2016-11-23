f0cb69b694d1daf09647437bbb9064bc6c397a8e
Kooscii <mushkoala@gmail.com>
2016-11-22 20:57:22 -0500

    updated check_answer.py

--- LAB2/tg1553/check_answer.py
+++ LAB2/tg1553/check_answer.py
@@ -1,4 +1,5 @@
 import os
+from math import log2
 
 try:
     os.remove('trace_out.txt')
@@ -12,12 +13,19 @@ g = open('trace.txt.out', 'r')
 o = open('trace_out.txt', 'w')
 o2 = open('check.csv', 'w')
 
-L1blocksize=8
-L1setsize=2
-L1size=32
-L2blocksize=16
-L2setsize=8
-L2size=64
+L1blocksize = 8
+L1setsize = 1
+L1size = 16
+L2blocksize = 16
+L2setsize = 4
+L2size = 32
+
+offsetbits1 = int(log2(L1blocksize))
+indexbits1 = int(log2(L1size * 1024 / L1setsize) - offsetbits1)
+tagbits1 = int(32 - indexbits1 - offsetbits1)
+offsetbits2 = int(log2(L2blocksize))
+indexbits2 = int(log2(L2size * 1024 / L2setsize) - offsetbits2)
+tagbits2 = int(32 - indexbits2 - offsetbits2)
 
 op = []
 L1_tag = []
@@ -35,12 +43,12 @@ for line in f:
     op.append(line[0])
     addr_hex = line[4:-1]
     addr_bin = bin(int(addr_hex, 16))[2:].zfill(32)
-    L1_tag.append(int(addr_bin[0:18], 2))
-    L1_index.append(int(addr_bin[18:18 + 11], 2))
-    L1_offset = int(addr_bin[18 + 11:], 2)
-    L2_tag.append(int(addr_bin[0:19], 2))
-    L2_index.append(int(addr_bin[19:19 + 9], 2))
-    L2_offset = int(addr_bin[19 + 9:], 2)
+    L1_tag.append(int(addr_bin[0:tagbits1], 2))
+    L1_index.append(int(addr_bin[tagbits1:tagbits1 + indexbits1], 2))
+    L1_offset = int(addr_bin[tagbits1 + indexbits1:], 2)
+    L2_tag.append(int(addr_bin[0:tagbits2], 2))
+    L2_index.append(int(addr_bin[tagbits2:tagbits2 + indexbits2], 2))
+    L2_offset = int(addr_bin[tagbits2 + indexbits2:], 2)
 
     o.write('%s 0x%s L1: %10s %8s %6s %4s  L2:  %10s %8s %6s %4s\n' % (
         op[-1], addr_hex.zfill(8), str(L1_tag[-1]), str(L1_index[-1]), str(L1_offset), L1[-1], str(L2_tag[-1]),
@@ -57,12 +65,19 @@ v2 = dict()
 evi1 = dict()
 evi2 = dict()
 
-o2.write(',,,%s,,,,,%s,,,,,\n' % ('L1', 'L2'))
-o2.write('%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,\n' % (
-    'no', 'check', 'R/W', 'index', 'tag', 
+o2.write(
+    (',,,%s,,after accessed,' + ',' * L1setsize + ',,%s,,after accessed,' + ',' * L2setsize + ',,\n') % ('L1', 'L2'))
+o2.write('%s,%s,%s,%s,%s,%s,' % ('no', 'check', 'R/W', 'index', 'tag', 'evicted'))
 
-    'way0', 'expect', 'answer', 'index', 'tag', 'way0', 'way1', 'way2', 'way3',
-    'expect', 'answer'))
+way1 = tuple(['way' + str(i) for i in range(0, L1setsize)])
+o2.write('%s,' * L1setsize % way1)
+
+o2.write('%s,%s,%s,%s,%s,' % ('expect', 'answer', 'index', 'tag', 'evicted'))
+
+way2 = tuple(['way' + str(i) for i in range(0, L2setsize)])
+o2.write('%s,' * L2setsize % way2)
+
+o2.write('%s,%s,\n' % ('expect', 'answer'))
 
 for i in range(0, n):
     flag1 = 0
@@ -76,16 +91,16 @@ for i in range(0, n):
     try:
         c1[L1_index[i]]
     except:
-        c1[L1_index[i]] = [0]
-        v1[L1_index[i]] = [0]
+        c1[L1_index[i]] = [0] * L1setsize
+        v1[L1_index[i]] = [0] * L1setsize
         # set1 = [0]
         evi1[L1_index[i]] = 0
 
     try:
         c2[L2_index[i]]
     except:
-        c2[L2_index[i]] = [0, 0, 0, 0]
-        v2[L2_index[i]] = [0, 0, 0, 0]
+        c2[L2_index[i]] = [0] * L2setsize
+        v2[L2_index[i]] = [0] * L2setsize
         # set2 = [0, 0, 0, 0]
         evi2[L2_index[i]] = 0
 
@@ -107,7 +122,7 @@ for i in range(0, n):
             expc1 = 'RM'
             c1[L1_index[i]][evi1[L1_index[i]]] = L1_tag[i]
             v1[L1_index[i]][evi1[L1_index[i]]] = 1
-            evi1[L1_index[i]] = (evi1[L1_index[i]] + 1) % 1
+            evi1[L1_index[i]] = (evi1[L1_index[i]] + 1) % L1setsize
 
     # L2
     if flag1 == 0:
@@ -128,7 +143,7 @@ for i in range(0, n):
                 expc2 = 'RM'
                 c2[L2_index[i]][evi2[L2_index[i]]] = L2_tag[i]
                 v2[L2_index[i]][evi2[L2_index[i]]] = 1
-                evi2[L2_index[i]] = (evi2[L2_index[i]] + 1) % 4
+                evi2[L2_index[i]] = (evi2[L2_index[i]] + 1) % L2setsize
 
     if expc1 == L1[i] and expc2 == L2[i]:
         chk = 'Correct'
@@ -139,9 +154,10 @@ for i in range(0, n):
     #     i, chk, op[i], L1_index[i], L1_tag[i], set1[0], expc1, L1[i]))
     # o2.write('%d,%d,%d,%d,%d,%d,%s,%s,\n' % (
     #     L2_index[i], L2_tag[i], set2[0], set2[1], set2[2], set2[3], expc2, L2[i]))
-    o2.write('%d,%s,%s,%d,%d,%d,%s,%s,' % (
-        i, chk, op[i], L1_index[i], L1_tag[i], c1[L1_index[i]][0], expc1, L1[i]))
-    o2.write('%d,%d,%d,%d,%d,%d,%s,%s,\n' % (
-        L2_index[i], L2_tag[i], c2[L2_index[i]][0], c2[L2_index[i]][1], c2[L2_index[i]][2], c2[L2_index[i]][3], expc2, L2[i]))
+    o2.write('%d,%s,%s,%d,%d,%d,' % (i, chk, op[i], L1_index[i], L1_tag[i], evi1[L1_index[i]]))
+    o2.write('%d,' * L1setsize % tuple(c1[L1_index[i]]))
+    o2.write('%s,%s,%d,%d,%d,' % (expc1, L1[i], L2_index[i], L2_tag[i], evi2[L2_index[i]]))
+    o2.write('%d,' * L2setsize % tuple(c2[L2_index[i]]))
+    o2.write('%s,%s,\n' % (expc2, L2[i]))
 
 f.close()
